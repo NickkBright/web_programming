cb5c72c7bef9a63da2cb00e1d14a6d3fad473bf3
Nick Bright <Nick Bright>
2016-11-08 01:16:35 +0300

    Fixed errors being ignored

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,14 @@
 "use strict";
-var userSearch = function(username, APIkey, limit){
+$(document).ready(function(){
+
+
+	$("#top-artist-search").on('submit', function(event){
+		event.preventDefault();
+		var username = $("#search-user").val();
+        var APIkey = "a5ccaac44e419797359236f7632f2950";
+        var limit = $("#set-limit").val();
+		if (limit ==""){limit = 50};
+        var userSearch = function(username, APIkey, limit){
         $(".json-info").remove();
         $(".error").remove();
         $(".user-not-found").remove();
@@ -12,19 +21,13 @@ var userSearch = function(username, APIkey, limit){
                 var responseData = JSON.parse(xhr.responseText);
                 resolve(responseData);
             }
-            else {reject(status)};
+        };
+        xhr.onerror = function(){
+            reject(status);
         }
         xhr.send();
         });
 };
-$(document).ready(function(){
-	$("#top-artist-search").on('submit', function(event){
-        
-		event.preventDefault();
-		var username = $("#search-user").val();
-        var APIkey = "a5ccaac44e419797359236f7632f2950";
-        var limit = $("#set-limit").val();
-		if (limit ==""){limit = 50};
 		userSearch(username,APIkey,limit).then(function(responseData){
 				if((responseData.error==6)||(responseData.topartists.artist.length==0)){
                     addNewDiv("user-not-found");
