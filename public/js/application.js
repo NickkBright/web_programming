ed85dd47e2b90220568dbf9835eb2725324871bd
Nick Bright <Nick Bright>
2016-11-07 21:56:53 +0300

    Error div added

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,8 @@
 "use strict";
 $(document).ready(function(){
-	var timer;
 	$("#top-artist-search").on('submit', function(event){
+        
 		event.preventDefault();
-		$(".json-info").remove();
-    	addNewDiv();
-
 		var username = $("#search-user").val();
         var APIkey = "a5ccaac44e419797359236f7632f2950";
         var limit = $("#set-limit").val();
@@ -14,40 +11,44 @@ $(document).ready(function(){
 	});
 });
 
-function addNewDiv(){
+function addNewDiv(nameOfClass){
     var newDiv = document.createElement("div");
-      newDiv.className = "json-info";
+      newDiv.className = nameOfClass;
         newDiv.innerHTML = "";
-
     var beforeDiv = document.getElementById("row");
     document.body.insertBefore(newDiv, beforeDiv);
 }
 
 function userSearch(username, APIkey, limit){
+        $(".json-info").remove();
+        $(".error").remove();
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user='+username+'&api_key='+APIkey+'&limit='+limit+'&format=json', true); 
 		xhr.onload = function(){
-			if(this.status == 422){
-				alert("422 Unprocessable Entity");
-			}
-			if(this.status == 404){
-				alert("404 not found");
-			}
 			if(this.status == 200){
 				var responseData = JSON.parse(xhr.responseText);
 				if(responseData.topartists.artist.length==0){alert("User Not Found! Try again")};
-				console.log(responseData.topartists.artist);
+				console.log(responseData);
+                
 				for(var i = 0; i < limit; ++i){
 					responseData.topartists.artist[i].image = Object.keys(responseData.topartists.artist[i].image[2]).map(function(key){return responseData.topartists.artist[i].image[2][key]});
 				}
-
-				var source = $("#lastfm-template").html();
-				var template = Handlebars.compile(source);
+                addNewDiv("json-info");
+				let source = $("#lastfm-template").html();
+				let template = Handlebars.compile(source);
 				$('.json-info').append(template({objects:responseData.topartists.artist}));
 			}
+            else {
+                
+            }
 		}
-		xhr.onerror = function(){
-			console.log("BAD: " + this.status);
-		}
+        xhr.onerror = function(){
+            addNewDiv("error");
+                let source = $("#error-template").html();
+				let template = Handlebars.compile(source);
+                $('.error').append(template);
+                setTimeout(userSearch, 10000, username, APIkey, limit);
+        }
 		xhr.send();
+        
 };
