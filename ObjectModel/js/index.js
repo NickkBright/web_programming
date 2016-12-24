cbbf7b880602ffeaca2c9f5f4e36615e98867a8f
NickkBright <gressssssssky@gmail.com>
2016-12-24 14:22:55 +0300

    Beta

--- ObjectModel/js/index.js
+++ ObjectModel/js/index.js
@@ -1,65 +1,95 @@
-"use strict";
-$(document).ready(function(){
+'use strict';
 
+class Team{
+	constructor(name, surname, age) {
+    	this.name = name;
+        this.surname=surname;
+		this.age = age;
+		Team.id += 1;
+	}
+}
 
-	$("#top-artist-search").on('submit', function(event){
-		event.preventDefault();
-		var username = $("#search-user").val();
-        var APIkey = "a5ccaac44e419797359236f7632f2950";
-        var limit = $("#set-limit").val();
-		if (limit ==""){limit = 50};
-        var userSearch = function(username, APIkey, limit){
-        $(".json-info").remove();
-        $(".error").remove();
-        $(".user-not-found").remove();
-        return new Promise(function(resolve, reject){
-            var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user='+username+'&api_key='+APIkey+'&limit='+limit+'&format=json', true); 
-		xhr.onload = function(){
-            var status = xhr.status;
-            if(status == 200){
-                var responseData = JSON.parse(xhr.responseText);
-                resolve(responseData);
-            }
-        };
-        xhr.onerror = function(){
-            reject(status);
-        }
-        xhr.send();
-        });
-};
-		userSearch(username,APIkey,limit).then(function(responseData){
-				if((responseData.error==6)||(responseData.topartists.artist.length==0)){
-                    addNewDiv("user-not-found");
-                    let source = $("#not-found-template").html();
-				    let template = Handlebars.compile(source);
-                    $('.user-not-found').append(template);
-                };
-				console.log(responseData);
-                
-				for(var i = 0; i < limit; ++i){
-					responseData.topartists.artist[i].image = Object.keys(responseData.topartists.artist[i].image[2]).map(function(key){return responseData.topartists.artist[i].image[2][key]});
-				}
-                addNewDiv("json-info");
-				let source = $("#lastfm-template").html();
-				let template = Handlebars.compile(source);
-				$('.json-info').append(template({objects:responseData.topartists.artist}));
-        }, function(status){
-            addNewDiv("error");
-                let source = $("#error-template").html();
-				let template = Handlebars.compile(source);
-                $('.error').append(template);
-                setTimeout(userSearch, 10000, username, APIkey, limit);
-        });
-    });
-});
+class Unit extends Team{	
+	constructor(name, surname, age, weapon, spec,  lead, id) {
+		super(name, surname, age);
+		this.weapon = weapon;
+        this.spec=spec;
+		this.id = id;
+        this.lead = lead;
+	}
+}
 
-function addNewDiv(nameOfClass){
-    var newDiv = document.createElement("div");
-      newDiv.className = nameOfClass;
-        newDiv.innerHTML = "";
-    var beforeDiv = document.getElementById("row");
-    document.body.insertBefore(newDiv, beforeDiv);
+Team.id = 0;
+
+let units = [];
+let commanders = [];
+
+let addUnit = () => {
+	event.preventDefault();
+	let name = $('#_name').val();
+    let surname = $('#_surname').val();
+	let age = $('#_age').val();
+	let weapon = $('#_weapon').val();
+	let spec = $('#_spec').val();
+    let lead = $('#_lead').val();
+		
+	$('#unitForm')[0].reset();
+	let unit = new Unit(name, surname, age, weapon, spec, lead, Team.id);
+	units.push(unit);
+	console.log(units);
+	compileUnit(lead, Team.id);
 }
 
 
+
+let compileUnit = (lead) => {
+    console.log(lead);
+    let data = units;
+    let template;
+    let content;
+    if (lead==0){ template = Handlebars.compile($('#unit-template').html()); content = $('#unitList')[0]}
+    if (lead==1){ template = Handlebars.compile($('#commander-template').html()); content = $('#commList')[0]}
+    let result = template(data);
+    content.innerHTML = result;	
+}
+
+let deleteUnit = (lead, id) => {
+    let tmp;
+	for(let i = 0; i < units.length; i++) {
+        if(units[i].id == id) {
+            tmp = units[i].lead;
+            console.log(tmp);
+            units.splice(i, 1);
+            break;
+        }
+    }
+    compileUnit(tmp);
+}
+
+let editUnit = (id) => {
+	for(let i = 0; i < units.length; i++) {
+        if(units[i].id == id) {
+			$('#_name').val(units[i].name);
+            $('#_surname').val(units[i].surname);
+			$('#_age').val(units[i].age);
+			$('#_weapon').val(units[i].weapon);
+			$('#_spec').val(units[i].spec);
+            $('#_lead').val(units[i].lead)
+            break;
+        }
+    }
+    addUnit();
+}
+
+$(document).on('click', '.addUnit', function(){
+    addUnit();
+});
+
+$(document).on('click', '.deleteUnit', function(){
+    deleteUnit(this.lead, this.id);
+});
+
+$(document).on('click', '.editUnit', function(){
+	editUnit(this.id);
+    deleteUnit(this.lead, this.id);
+});
